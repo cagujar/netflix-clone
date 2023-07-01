eb827a7932770b0b676ea58c23b01ecba96b4b0e
Chrissa Agujar <55868590+cagujar@users.noreply.github.com>
2023-07-01 14:22:20 +0800

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBWv8ucYt0tAunKbwFYEFblndsCr_uNS00",
+    authDomain: "netflix-clone-f02d8.firebaseapp.com",
+    projectId: "netflix-clone-f02d8",
+    storageBucket: "netflix-clone-f02d8.appspot.com",
+    messagingSenderId: "359994311773",
+    appId: "1:359994311773:web:ba358a16f3c9ef2fa639b4"
+  };
